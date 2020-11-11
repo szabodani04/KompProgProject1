4370b61a52723dbe72eec6f70791bc82cf0ec2fc
szabodani04 <danielmark0401@gmail.com>
2020-11-11 19:36:02 +0100

    értékelőlap elkészítése és a tétel kidolgozásának elkezdése

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+function Submit(){
+    var a = parseInt(document.getElementById("fogalomismeret").value);
+    var b = parseInt(document.getElementById("fogalomdefinialas").value);
+    var c = parseInt(document.getElementById("tartalom").value);
+    var d = parseInt(document.getElementById("elvek").value);
+    var e = parseInt(document.getElementById("osszefugg").value);
+    var f = parseInt(document.getElementById("idobeosztas").value);
+    var g = parseInt(document.getElementById("felepitettseg").value);
+    var h = parseInt(document.getElementById("eloadas").value);
+    var j = parseInt(document.getElementById("szaknyelv").value);
+    var osszeg = a + b + c + d + e + f + g + h + j;
+    var szazalek = osszeg/30*100;
+    document.getElementById("eredmeny").innerHTML = "Eredmény: " + osszeg +" pont    " + szazalek + "%";
+    if (szazalek<25){
+        document.getElementById("erdemjegy").innerHTML = "1 Elégtelen";
+        document.getElementById("erdemjegy").style.color = "red";
+    }
+    else if (szazalek < 40){
+        document.getElementById("erdemjegy").innerHTML = "2 Elégséges";
+        document.getElementById("erdemjegy").style.color = "orange";
+    }
+    else if (szazalek < 60){
+        document.getElementById("erdemjegy").innerHTML = "3 Közepes";
+        document.getElementById("erdemjegy").style.color = "yellow";
+    }
+    else if (szazalek < 80){
+        document.getElementById("erdemjegy").innerHTML = "4 Jó";
+        document.getElementById("erdemjegy").style.color = "lightblue";
+    }
+    else{
+        document.getElementById("erdemjegy").innerHTML = "5 Jeles";
+        document.getElementById("erdemjegy").style.color = "lightgreen";
     }
 }
